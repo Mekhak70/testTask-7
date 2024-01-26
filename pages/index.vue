4e8e7e7f726191c4fb206cc865099ed390d7c084
Aren-Chilingaryan <arenchilingaryan88@gmail.com>
2024-01-26 21:15:30 +0400

    init

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,25 @@
-
 <template>
   <div>
-    <Banner/>
+    <Banner />
+    <SectionOne />
   </div>
 </template>
 
-
 <script>
+// Import both components. Adjust paths as necessary.
+import Banner from '@/components/banner.vue';
+import SectionOne from '@/components/SectionOne.vue';
+
 export default {
-    name: 'IndexPage',
+  name: 'IndexPage',
+  components: {
+    // Register both components for use in the template
+    Banner,
+    SectionOne
+  }
 }
 </script>
+
+<style>
+/* Styles for index.vue */
+</style>
