8a878874f48bf7aa9b2dfa6fb145861b82ae72b4
Mekhak70 <mexakchilingaryan3@gmail.com>
2024-01-26 22:06:52 +0400

    public

---
+++ components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="header">
+     <div class="logo">
+        <!-- <img src="~/public/LOGO.svg" alt="My Image Alt Text"> -->
+     </div>
+     <div>
+        <ul>
+
+        </ul>
+     </div>
+    </div>
+  </template>
+
+  <script lang="ts" setup>
+
+  // Script content remains empty for this example
+  </script>
+
+  <style>
+  .header {
+    background-color: #009688; /* Teal background */
+    color: white; /* White text */
+    padding: 20px; /* Some padding */
+    text-align: center; /* Centered text */
+  }
+
+  </style>
